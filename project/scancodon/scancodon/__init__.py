deb414d6cb9a6555dc9a6392625935058e43e205
Owen Lutwyche <56270422+OwenLutwyche@users.noreply.github.com>
2026-07-01 14:25:02 -0700

    basis of scrublet
    currently has an error when initializing CSRMatrix

--- project/scancodon/scancodon/__init__.py
+++ project/scancodon/scancodon/__init__.py
@@ -431,6 +431,74 @@ class Preprocessing:
             sc.preprocessing.highly_variable_genes(adata, n_top_genes=20, flavor='seurat')
         if subset: adata._inplace_subset_var(adata.var['highly_variable'])
 
+    def scrublet(
+        self, 
+        data, 
+        sim_doublet_ratio=2.0, 
+        expected_doublet_rate=0.1, 
+        stdev_doublet_rate=0.02, 
+        random_state=0, 
+        inplace=True,
+        **kwargs
+    ):
+        adata = data if inplace else data.copy()
+        X = self._get_x(adata)
+
+        if CODON_AVAILABLE:
+            print("[INFO] Running native scancodon Scrublet")
+            
+            # check sparse matrix
+            if not sp_sparse.issparse(X):
+                X = sp_sparse.csr_matrix(X)
+            elif not isinstance(X, sp_sparse.csr_matrix):
+                X = X.tocsr()
+
+            if len(X.indptr) != X.shape[0] + 1:
+                print(f"CRITICAL ERROR: Matrix has {X.shape[0]} rows but indptr has {len(X.indptr)} elements.")
+                # Attempt to fix it
+                X = X.tocsr() # Force a clean rebuild
+                if len(X.indptr) != X.shape[0] + 1:
+                    raise ValueError("Matrix indptr is structurally invalid.")
+                
+            # force 64-bit strict types for the Codon bridge, they will be converted to a codon CSRMatrix later
+            data_64 = np.asarray(X.data, dtype=np.float64)
+            indices_64 = np.asarray(X.indices, dtype=np.int64)
+            indptr_64 = np.asarray(X.indptr, dtype=np.int64)
+            
+            # native dispatcher call
+            scores = scancodon_native.scrublet(
+                data_64, 
+                indices_64, 
+                indptr_64, 
+                X.shape[0], 
+                X.shape[1],
+                float(sim_doublet_ratio),
+                float(expected_doublet_rate),
+                float(stdev_doublet_rate),
+                int(random_state)
+            )
+            
+            # write back to anndata
+            adata.obs['doublet_score'] = scores
+            # TODO add predicted_doublets_ and uns
+            
+        else:
+            print("[INFO] Falling back to scanpy.pp.scrublet")
+            sc.pp.scrublet(
+                adata, 
+                sim_doublet_ratio=sim_doublet_ratio,
+                expected_doublet_rate=expected_doublet_rate,
+                stdev_doublet_rate=stdev_doublet_rate,
+                random_state=random_state,
+                **kwargs
+            )
+
+        return None if inplace else adata
+    
+
+
+
+
     def pca(self, data, n_comps=50, zero_center=True, **kwargs):
         t0 = time.time()
         adata = data
